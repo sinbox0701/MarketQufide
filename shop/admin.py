609914166d5b7cd4a19109c97fe3564d5e31e357
sinbox0701 <53805569+sinbox0701@users.noreply.github.com>
2019-08-12 15:30:32 +0900

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-class CategoryAdmin(admin.ModelAdmin): # 카테고리 옵션 클래스
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)} # slug 필드는 name 필드의 값에 따라 자동 설정 가능
 
-admin.site.register(Category, CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'categories', 'price', 'stock', 'available_display', 'available_order', 'created', 'updated']
@@ -13,7 +9,7 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)} # slug 필드는 name 필드의 값에 따라 자동 설정 가능
     list_editable = ['price', 'stock', 'available_display', 'available_order'] # 주요값 바로 변경 가능
 
-admin.site.register(Product, ProductAdmin)
+
 
 
 admin.site.register(Banner)
