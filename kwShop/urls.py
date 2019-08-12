36d95168106b092f257eb8b28205ffed44ec1612
dev-ujin <ujiiin518@gmail.com>
2019-08-12 16:22:21 +0900

    qna

--- kwShop/urls.py
+++ kwShop/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('coupon/', include('coupon.urls')),
     path('order/', include('order.urls')),
+    path('qna/', include('qna.urls')),
     path('', include('shop.urls')),
 ]
