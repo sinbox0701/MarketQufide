173c2b84e175ae2b8ddd72f55c06fb2a79dd9257
sinbox0701 <53805569+sinbox0701@users.noreply.github.com>
2019-08-13 13:36:26 +0900

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 app_name = 'shop'
 
 urlpatterns = [
-    path('', category, name='product_all'), # 카테고리 선택 없이 상품 전체 노출
+    #path('', category, name='product_all'), # 카테고리 선택 없이 상품 전체 노출
     path('<slug:category_slug>/', category, name='category'), # 카테고리 선택
     path('', home, name='product_all'),
     #path('', search),
