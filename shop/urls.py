2f6160cc9a8e96490859a2f80bda0ac48d06f57c
dev-ujin <ujiiin518@gmail.com>
2019-08-13 17:28:19 +0900

    migrations

--- shop/urls.py
+++ shop/urls.py
@@ -6,14 +6,19 @@ from django.views.generic import TemplateView
 app_name = 'shop'
 
 urlpatterns = [
-    path('', category, name='product_all'), # 카테고리 선택 없이 상품 전체 노출
-    path('<slug:category_slug>/', category, name='category'), # 카테고리 선택
+    #path('', category, name='product_all'), # 카테고리 선택 없이 상품 전체 노출
     path('', home, name='product_all'),
-    #path('', search),
+    path('best_item/', best_item, name="best_item"),
+    path('new_item/', new_item, name="new_item"),
+    path('frugal_shopping/', frugal_shopping, name="frugal_shopping"),
+    path('exhibition/', exhibition, name="exhibition"),
+    path('event/', event, name="event"),
+    path('recipe/', recipe, name="recipe"),
     path('search/', search, name='search'),
+    path('<slug:category_slug>/', category, name='category'),  # 카테고리 선택
     #path('', product_in_category, name='product_all'), # 카테고리 선택 없이 상품 전체 노출
-    path('<slug:category_slug>/', product_in_category, name='product_in_category'), # 카테고리 선택
-    path('<int:id>/<product_slug>/', product_detail, name='product_detail'), # 상품 상세 페이지
+    #path('<slug:category_slug>/', product_in_category, name='product_in_category'), # 카테고리 선택
+    path('<int:id>/<product_slug>/', product_detail, name='product_detail'), # 상품 상세 페이
     path('<int:id>/<product_slug>/comment/', comment, name='comment'),
     path('<int:id>/<product_slug>/comment/delete', delete_comment, name="delete_comment"),
     path('<int:id>/<product_slug>/comment/update', update_comment, name="update_comment"),
