eecc9763f5133f2a61c3797699c6468722e6a4c3
Yunkiee <clova9330@gmail.com>
2019-10-11 16:07:52 +0900

    연동로그인

--- members/admin.py
+++ members/admin.py
@@ -3,7 +3,6 @@ from .models import User,SmsSend, Marketing, Address
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(SmsSend)
 admin.site.register(Marketing)
 admin.site.register(Address)
 
